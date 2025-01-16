d4c4a8aa92440525a2748f9512a80ff1bd1b6bce
Cyrille-18 <cyrillepio@hotmail.com>
2025-01-16 15:30:46 +0000

    Feat: Implements Update Method
    create Put method in controller

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -25,6 +25,7 @@ namespace api.Controllers
             var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
             return Ok(stocks);
         }
+
         [HttpGet("{id}")]
         public IActionResult GetById([FromRoute] int id)
         {
@@ -37,6 +38,7 @@ namespace api.Controllers
             return Ok(stock.ToStockDto());
 
         }
+
         [HttpPost]
         public IActionResult Create([FromBody] CreateStockRequestDto StockDto)
         {
@@ -44,6 +46,29 @@ namespace api.Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
