2cb479c144947de63299491c3a4802f602d95284
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-09 23:42:53 +0000

    Feat: Add model validation to Comment and Stock DTOs and enforce integer route constraints in controllers

--- Dtos/Comment/CreateCommentDto.cs
+++ Dtos/Comment/CreateCommentDto.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace api.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 characters")]
+        [MaxLength(280, ErrorMessage = "Title must be at most 50 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 characters")]
+        [MaxLength(280, ErrorMessage = "Content must be at most 50 characters")]
         public string Content { get; set; } = string.Empty;
     }
 }
