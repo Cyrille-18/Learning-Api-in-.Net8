6887c7d644d1652676c2c36b357fe8b3f5c7bef5
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-22 23:50:30 +0000

    Feat: Add Table attribute to Stock model and Portfolios property for user stock associations

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace api.Models
 {
+    [Table("Stocks")]
     public class Stock
     {
         public int Id { get; set; }
@@ -19,5 +20,7 @@ namespace api.Models
         public long MarketCap { get; set; }
 
         public List<Comment> comments { get; set; } = new List<Comment>();
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
+
     }
 }
