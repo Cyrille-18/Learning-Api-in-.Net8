272e101f605fec0541f21a5ca7e9c7200b6ecd2e
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-25 20:27:55 +0000

    feat: Implement GetUserPortfolio method in PortfolioController for retrieving user portfolios

--- Controllers/PortfolioController.cs
+++ Controllers/PortfolioController.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Extensions;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -10,6 +15,25 @@ namespace api.Controllers
     [ApiController]
     public class PortfolioController : ControllerBase
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        private readonly IPortfolioRepository _portfolioRepo;
 
+        PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepo, IPortfolioRepository portfolioRepo)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo;
+            _portfolioRepo = portfolioRepo;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var Username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(Username);
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+            return Ok(userPortfolio);
+        }
     }
 }
